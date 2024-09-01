8c0ab9cb2af329dbae0caf995505d3054399d77b
rift <117926989+Riftriot@users.noreply.github.com>
2024-09-01 00:20:44 -0500

    inital commit

---
+++ src/codegen.rs
@@ -0,0 +1,62 @@
+use hmac::{Hmac, Mac};
+use sha1::Sha1;
+
+type HmacSha1 = Hmac<Sha1>;
+
+const CODE_GRANULARITY_MS: u64 = 1000 * 60;
+
+pub fn generate_fl_code(shared_secret: String, custom_timestamp: u64) -> String {
+
+    let key = shared_secret.trim();
+
+    let timestamp_ms: u64 = custom_timestamp * 1000;
+
+    let code_validity_ms = 1000u64 * 60 * 60; // 1 hour
+    let interval = timestamp_ms / code_validity_ms;
+    let interval_beginning_timestamp_ms = interval * code_validity_ms;
+    let adjusted_timestamp = interval_beginning_timestamp_ms / CODE_GRANULARITY_MS;
+    let big_endian_timestamp = adjusted_timestamp.to_be_bytes();
+
+    let mut mac = HmacSha1::new_from_slice(key.as_bytes()).unwrap();
+    mac.update(&big_endian_timestamp[..]);
+    let digest = mac.finalize().into_bytes();
+
+    let offset = (digest.last().expect("digest is not empty") & 0xf) as usize;
+    // read as i32 and clear sign bit
+    let result = i32::from_be_bytes(digest[offset..][..4].try_into().unwrap()) & 0x7fffffff;
+
+    let valid_from_ms = interval_beginning_timestamp_ms;
+    let valid_to_ms = valid_from_ms + code_validity_ms;
+
+    let code: i32 = result % 1000000;
+    let remaining_sec = (valid_to_ms - timestamp_ms) / 1000;
+    // println!(
+    //     "Expires in: {:02}:{:02} ({} sec)",
+    //     remaining_sec / 60,
+    //     remaining_sec % 60,
+    //     remaining_sec
+    // );
+    format!("{:06}", code)
+}
+
+pub fn remaining_time(custom_timestamp: u64) -> u64 {
+
+    let timestamp_ms: u64 = custom_timestamp * 1000;
+
+    let code_validity_ms = 1000u64 * 60 * 60; // 1 hour
+    let interval = timestamp_ms / code_validity_ms;
+    let interval_beginning_timestamp_ms = interval * code_validity_ms;
+
+    let valid_from_ms = interval_beginning_timestamp_ms;
+    let valid_to_ms = valid_from_ms + code_validity_ms;
+
+    let remaining_sec = (valid_to_ms - timestamp_ms) / 1000;
+    
+    remaining_sec
+}
+
+
+pub fn float_from_time(seconds_left: u64) -> f32 {
+    let hours_left = seconds_left as f32 / 3600.0;
+    hours_left
+}
---
+++ src/main.rs
@@ -0,0 +1,123 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] pub mod codegen;
+
+// hide console window on Windows in release
+use std::time::{SystemTime, UNIX_EPOCH};
+use eframe::egui::{self, FontFamily};
+
+use egui::FontFamily::Proportional;
+use codegen::{generate_fl_code, remaining_time, float_from_time};
+use egui_modal::Modal;
+fn main() -> eframe::Result {
+    let icon = include_bytes!("../logo.png");
+    let image = image::load_from_memory(icon).expect("Failed to open icon path").to_rgba8();
+    let (icon_width, icon_height) = image.dimensions();
+
+
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([620.0, 280.0]).with_icon(egui::IconData { 
+            rgba: image.into_raw(), 
+            width: icon_width, 
+            height: icon_height,
+        }),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Family Link Codegen",
+        options,
+        
+        Box::new(|cc| {
+            // This gives us image support:
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+
+            Ok(Box::<MyApp>::default())
+        }),
+    )
+}
+
+struct MyApp {
+    shared_secret: String,
+    custom_timestamp: bool,
+    selected_timestamp: String,
+    validated_selected_timestamp: u64
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            shared_secret: "".to_owned(),
+            custom_timestamp: false,
+            selected_timestamp: "222".to_string(),
+            validated_selected_timestamp: 0
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let modal = Modal::new(ctx, "my_modal");
+        
+        modal.show(|ui| {
+            // these helper functions help set the ui based on the modal's
+            // set style, but they are not required and you can put whatever
+            // ui you want inside [`.show()`]
+            modal.title(ui, "Error");
+            modal.frame(ui, |ui| {
+                modal.body(ui, "Invalid Unix timestamp submitted.");
+            });
+        });
+
+        if !self.custom_timestamp {
+            self.selected_timestamp = SystemTime::now().duration_since(UNIX_EPOCH).expect("crazy errr").as_secs().to_string();
+            self.validated_selected_timestamp = SystemTime::now().duration_since(UNIX_EPOCH).expect("crazy errr").as_secs();
+        }
+
+        let font_id = egui::FontId::new(48.0, FontFamily::Monospace);
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Family Link Codegen");
+            ui.horizontal(|ui| {
+                ui.label("Shared secret: ");
+                ui.add(egui::TextEdit::singleline(&mut self.shared_secret).hint_text("Paste your shared secret here"));
+            });
+            ui.add(egui::Checkbox::new(&mut self.custom_timestamp, "Generate for future time"));
+            ui.add(egui::Hyperlink::from_label_and_url("How do I get a Unix timestamp?", "https://www.unixtimestamp.com/"));
+            ui.horizontal(|ui| {
+                ui.label("Unix timestamp: ");
+                ui.add_enabled(self.custom_timestamp, egui::TextEdit::singleline(&mut self.selected_timestamp).hint_text("Enter a unix timestamp here"));
+            });
+            if ui.add_enabled(self.custom_timestamp,egui::Button::new("Submit")).clicked() {
+                match &self.selected_timestamp {
+                    num=>match num.parse::<u64>() {
+                        Ok(num)=> {
+                            self.validated_selected_timestamp = num
+                        }
+                        Err(_)=> {
+                            modal.open()
+                        }
+                    },
+                };
+            }
+
+            ui.label(
+                egui::RichText::new(format!("{}", generate_fl_code(self.shared_secret.clone(), self.validated_selected_timestamp)))
+                    .font(font_id)
+            );
+
+            ui.add(egui::ProgressBar::new(float_from_time(remaining_time(self.validated_selected_timestamp))).text(
+                format!(
+                    "Expires in: {:2}:{:02}",
+                    remaining_time(self.validated_selected_timestamp) / 60,
+                    remaining_time(self.validated_selected_timestamp) % 60,
+                )
+            ));
+
+
+            egui::TopBottomPanel::bottom("my_panel").show(ctx, |ui| {
+                    ui.add(egui::Hyperlink::from_label_and_url("Made by the amazing team at AntiLink", "https://github.com/anti-link"));
+                    ui.add(egui::Hyperlink::from_label_and_url("Source code", "https://github.com/anti-link/fl_codegen_gui"));
+             });
+
+             ui.ctx().request_repaint();
+        });
+    }
+}
